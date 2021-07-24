c4d69bb9d607876c3c5f1e7d15cd6b8ad85f0d34
belkoweb <belkoweb9718@gmail.com>
2021-07-24 12:20:34 +0100

    Menu role management

--- src/main/java/ma/zs/generator/engine/bean/MenuRole.java
+++ src/main/java/ma/zs/generator/engine/bean/MenuRole.java
@@ -2,6 +2,7 @@ package ma.zs.generator.engine.bean;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 @Entity
 public class MenuRole {
 	@ManyToOne
--- src/main/java/ma/zs/generator/engine/dao/MenuDao.java
+++ src/main/java/ma/zs/generator/engine/dao/MenuDao.java
@@ -3,6 +3,7 @@ package ma.zs.generator.engine.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ma.zs.generator.engine.bean.Menu;
@@ -10,5 +11,7 @@ import ma.zs.generator.engine.bean.Menu;
 @Repository
 public interface MenuDao extends JpaRepository<Menu, Long>{
 public Menu findByLibelle(String libelle);
+
+@Query("SELECT m FROM Menu m, Role r, Menu_Role mr  WHERE m.id = mr.menu and mr.role = r.id and r.name = ?1")
 public List<Menu> findAllByRoleName(String name);
 }
