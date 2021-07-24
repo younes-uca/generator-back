929496a2344d85df1503e7889b2b634db6ed27ac
belkoweb <belkoweb9718@gmail.com>
2021-07-24 13:09:31 +0100

    add ID to MenuRole

--- src/main/java/ma/zs/generator/engine/bean/MenuRole.java
+++ src/main/java/ma/zs/generator/engine/bean/MenuRole.java
@@ -1,10 +1,15 @@
 package ma.zs.generator.engine.bean;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 @Entity
 public class MenuRole {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 	@ManyToOne
 	private Menu menu;
 	@ManyToOne
